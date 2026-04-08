b3566f277306291b415b6720ac716330d5b69486
Alex Vito <eev1@pct.edu>
2026-04-08 15:20:06 -0400

    Lab13Corect

---
+++ Source/rollaball/rollaballGameMode.cpp
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "rollaballGameMode.h"
+#include "rollaballCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ArollaballGameMode::ArollaballGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+	
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_MyRollerPlayerController")); 
+	if (PlayerControllerBPClass.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerBPClass.Class;
+	}
+}
